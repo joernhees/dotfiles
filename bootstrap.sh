4e1ce6150aaa468fb54374cb2fa1fc459c137d44
Joern Hees <dev@joernhees.de>
2014-10-09 16:14:55 +0200

    modified bootstrap script so it symlinks instead of rsyncing and asks a lot more

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,37 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+path="${BASH_SOURCE%/*}"
 
-git pull origin master;
+cd "$path"
+path="$(pwd)"
+
+read -p "Do you want to run git pull origin master first? (y/n) "
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	git pull origin master
+fi
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
+	#rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+	#	--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+	cd ~
+	# link .files except .os, .DS_Store
+	find "$path" -type f -depth 1 -name '.*' -not \( -name '.osx' -or -name '.DS_Store' \) -exec ln -si {} \;
+	# link .directories
+	find "$path" -type d -depth 1 -name '.*' -not -name '.git' -exec ln -sFi {} \;
+	# link bin symlinks
+	rsync -anvh --no-perms "$path/bin" ./
+	read -p "Do you want to replace these symlinks in ~/bin? (y/n) "
+	echo ""
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		rsync -avh --no-perms "$path/bin" ./
+	fi
+	source ~/.bash_profile
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) "
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	doIt
+fi
+unset doIt path
