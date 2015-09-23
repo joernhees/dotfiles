ce3f78e2ebc92f8f283f3d7fb25ce99b56143abb
Joern Hees <dev@joernhees.de>
2015-09-23 18:15:09 +0200

    improved search for venv

--- bin/find_venv.sh
+++ bin/find_venv.sh
@@ -2,7 +2,7 @@
 # script that searches a python venv dir, identified by bin/activate
 #set -x
 
-for d in {.,..}/{venv,virt_env}/{,2.*/}bin/activate ; do
+for d in {.,..}/{venv,virt_env}/{,2*/,py2*/}bin/activate ; do
 	if [[ -f "$d" ]] ; then
 		# echo "echo activating venv: \"$d\""
 		echo "source \"$d\""
