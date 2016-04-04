c063ddb621673d4a152e05909ebb77b61e88f01e
Joern Hees <dev@joernhees.de>
2016-04-04 19:18:49 +0200

    added `venv` command to activate python virtual envs via find_venv.sh

---
+++ bin/find_venv.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# script that searches a python venv dir, identified by bin/activate
+#set -x
+
+for d in {.,..}/{venv,virt_env}/{,2.*/}bin/activate ; do
+	if [[ -f "$d" ]] ; then
+		# echo "echo activating venv: \"$d\""
+		echo "source \"$d\""
+		exit 0
+	fi
+done
+echo "echo \"none found\""
+exit 1
